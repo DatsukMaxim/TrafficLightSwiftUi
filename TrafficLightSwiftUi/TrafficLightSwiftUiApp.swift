ff64dc28b60affc6d989903e9f2f591e532a2b59
Datsuk Maxim <datsuk.job@gmail.com>
2022-09-10 23:28:09 +0700

    Initial Commit

---
+++ TrafficLightSwiftUi/TrafficLightSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUiApp.swift
+//  TrafficLightSwiftUi
+//
+//  Created by Maxim Datsuk on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
