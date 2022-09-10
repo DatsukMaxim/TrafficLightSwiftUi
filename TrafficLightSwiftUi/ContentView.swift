90ffa2d82e9d61740e60e6655d7f05dd9baaf845
Datsuk Maxim <datsuk.job@gmail.com>
2022-09-11 00:35:03 +0700

    init project

--- TrafficLightSwiftUi/ContentView.swift
+++ TrafficLightSwiftUi/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    let red = Color(.init(red: 1, green: 0, blue: 0, alpha: 0.3))
+    let yellow = Color(.init(red: 1, green: 1, blue: 0, alpha: 0.3))
+    let green = Color(.init(red: 0, green: 1, blue: 0, alpha: 0.3))
+    
+    @State private var buttonLabel = "START"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                Circle()
+                    .foregroundColor(red)
+                    .frame(width: 100, height: 100)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Circle()
+                    .foregroundColor(yellow)
+                    .frame(width: 100, height: 100)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Circle()
+                    .foregroundColor(green)
+                    .frame(width: 100, height: 100)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Spacer()
+                Button(action: { buttonLabel = "NEXT" }) {
+                    Text(buttonLabel)
+                        .font(.largeTitle.bold())
+                        .frame(width: 130, height: 30)
+                        .foregroundColor(.white)
+                        .padding(.init(
+                            top: 10, leading: 30, bottom: 10, trailing: 30
+                        ))
+                        .border(.white, width: 4)
+                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(.white, lineWidth: 10))
+                        .background(.blue)
+                        .cornerRadius(20)
+                }
+            }
+        }
     }
 }
 
